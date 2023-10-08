d282d2662ecf20dacbd36e64e0003582afabb0c1
Igor Durica <xduricai@stuba.sk>
2023-10-08 17:59:16 +0200

    Added heap

---
+++ heap/heap.go
@@ -0,0 +1,121 @@
+package heap
+
+import (
+	"errors"
+
+	. "golang.org/x/exp/constraints"
+)
+
+var ErrDeleteFromEmptyHeap = errors.New("Cannot delete from an empty heap")
+
+// MinHeap is a struct representing a MinHeap
+type MinHeap[T Ordered] struct {
+	length int
+	data   []T
+}
+
+// NewMinHeap is a constructor function for a MinHeap
+func NewMinHeap[T Ordered]() *MinHeap[T] {
+	return &MinHeap[T]{
+		length: 0,
+		data:   []T{},
+	}
+}
+
+// Length returns the number of items in the heap
+func (heap *MinHeap[T]) Length() int {
+	return heap.length
+}
+
+// Insert inserts a new value into the MinHeap
+func (heap *MinHeap[T]) Insert(value T) {
+	heap.data = append(heap.data, value)
+	heap.heapifyUp(heap.length)
+	heap.length++
+}
+
+// Delete deletes the last value in the MinHeap
+func (heap *MinHeap[T]) Delete() (T, error) {
+	if heap.length == 0 {
+		return *new(T), ErrDeleteFromEmptyHeap
+	}
+
+	out := heap.data[0]
+	heap.length--
+
+	if heap.length == 0 {
+		heap.data = []T{}
+		return out, nil
+	}
+
+	heap.data[0] = heap.data[heap.length]
+	heap.data = heap.data[:heap.length]
+	heap.heapifyDown(0)
+
+	return out, nil
+}
+
+// heapifyUp starts at the end of the heap and recursively bubbles the smallest value inside the heap to the top
+func (heap *MinHeap[T]) heapifyUp(idx int) {
+	if idx == 0 {
+		return
+	}
+
+	pIdx := parent(idx)
+	value := heap.data[idx]
+	parentValue := heap.data[pIdx]
+
+	if parentValue > value {
+		heap.data[idx] = parentValue
+		heap.data[pIdx] = value
+		heap.heapifyUp(pIdx)
+	}
+}
+
+// heapifyDown starts at the top of the heap and recursively bubbles a value to its appropriate position inside the heap
+func (heap *MinHeap[T]) heapifyDown(idx int) {
+	lIdx := leftChild(idx)
+	rIdx := rightChild(idx)
+
+	if idx >= heap.length || lIdx >= heap.length {
+		return
+	}
+
+	value := heap.data[idx]
+	lValue := heap.data[lIdx]
+
+	if rIdx == heap.length {
+		if value > lValue {
+			heap.data[idx] = lValue
+			heap.data[lIdx] = value
+		}
+		return
+	}
+
+	rValue := heap.data[rIdx]
+
+	if lValue > rValue && value > rValue {
+		heap.data[idx] = rValue
+		heap.data[rIdx] = value
+		heap.heapifyDown(rIdx)
+	} else if value > lValue {
+		heap.data[idx] = lValue
+		heap.data[lIdx] = value
+		heap.heapifyDown(lIdx)
+	}
+}
+
+// parent uses an index of an item in the heap to calculate the index of its parent
+func parent(idx int) int {
+	return (idx - 1) / 2
+}
+
+// leftChild uses an index of an item in the heap to calculate the index of its left child
+func leftChild(idx int) int {
+	return idx*2 + 1
+}
+
+// rightChild uses an index of an item in the heap to calculate the index of its right child
+func rightChild(idx int) int {
+	return idx*2 + 2
+}
---
+++ tests/heap_test.go
@@ -0,0 +1,36 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/xduricai/dsa/heap"
+)
+
+func TestHeap(t *testing.T) {
+	values := []int{1, 3, 4, 5, 7, 8, 70, 100}
+	heap := heap.NewMinHeap[int]()
+
+	heap.Insert(5)
+	heap.Insert(3)
+	heap.Insert(70)
+	heap.Insert(100)
+	heap.Insert(4)
+	heap.Insert(1)
+	heap.Insert(8)
+	heap.Insert(7)
+
+	if heap.Length() != 8 {
+		t.Errorf("Heap failed, incorrect length")
+	}
+
+	for _, value := range values {
+		res, err := heap.Delete()
+		if res != value || err != nil {
+			t.Errorf("Heap failed, incorrect order of elements")
+		}
+	}
+
+	if heap.Length() != 0 {
+		t.Errorf("Heap failed, incorrect length")
+	}
+}
