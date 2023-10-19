28b2f5856174e211c595aa73b3ad9a0f9ffef921
Igor Durica <xduricai@stuba.sk>
2023-10-19 19:23:03 +0200

    Added DFS to graph matrix search :3c

--- graphs/graph_matrix_search.go
+++ graphs/graph_matrix_search.go
@@ -5,10 +5,53 @@ import "github.com/xduricai/dsa/lists"
 // AdjecencyMatrix is a type alias for a 2D array of integers
 type AdjecencyMatrix = [][]int
 
-// Bfs uses breadth-first search to find the path from a source node to a target node in a graph, returns a slice with the order of nodes or an empty slice if no path exists
-func Bfs(graph AdjecencyMatrix, source int, target int) []int {
-	seen := make([]bool, len(graph))
-	previous := make([]int, len(graph))
+// walk checks whether a vertex of a graph is our destination and subsequently walks to its neighbours
+func walk(graph *AdjecencyMatrix, current int, target int, seen *[]bool, path *[]int) bool {
+	if (*seen)[current] {
+		return false
+	}
+	if current == target {
+		*path = append((*path), target)
+		return true
+	}
+
+	(*seen)[current] = true
+	*path = append((*path), current)
+	adjecent := (*graph)[current]
+
+	for i := 0; i < len(adjecent); i++ {
+		if adjecent[i] == 0 {
+			continue
+		}
+		foundTarget := walk(graph, i, target, seen, path)
+
+		if foundTarget {
+			return true
+		}
+	}
+	*path = (*path)[:len(*path)-1]
+
+	return false
+}
+
+// DFS uses depth-first search to find the path from a source node to a target node in a graph, returns a slice with the order of nodes or an empty slice if no path exists
+func DFS(graph *AdjecencyMatrix, source int, target int) []int {
+	path := []int{}
+	seen := make([]bool, len(*graph))
+	queue := lists.NewQueue[int]()
+	queue.Enqueue(source)
+
+	pathExists := walk(graph, source, target, &seen, &path)
+	if pathExists {
+		return path
+	}
+	return []int{}
+}
+
+// BFS uses breadth-first search to find the path from a source node to a target node in a graph, returns a slice with the order of nodes or an empty slice if no path exists
+func BFS(graph *AdjecencyMatrix, source int, target int) []int {
+	seen := make([]bool, len(*graph))
+	previous := make([]int, len(*graph))
 	for idx := range previous {
 		previous[idx] = -1
 	}
@@ -23,7 +66,7 @@ func Bfs(graph AdjecencyMatrix, source int, target int) []int {
 		if current == target {
 			break
 		}
-		adjecent := graph[current]
+		adjecent := (*graph)[current]
 
 		for i := 0; i < len(adjecent); i++ {
 			if adjecent[i] == 0 || seen[i] {
--- tests/graph_matrix_search_test.go
+++ tests/graph_matrix_search_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/xduricai/dsa/graphs"
 )
 
-func TestGraphMatrixBfs(t *testing.T) {
+func TestGraphMatrixSearch(t *testing.T) {
 	matrix := graphs.AdjecencyMatrix{
 		{0, 3, 1, 0, 0, 0, 0},
 		{0, 0, 0, 0, 1, 0, 0},
@@ -17,21 +17,34 @@ func TestGraphMatrixBfs(t *testing.T) {
 		{0, 0, 0, 1, 0, 0, 1},
 	}
 	expected := []int{0, 1, 4, 5, 6}
-	path := graphs.Bfs(matrix, 0, 6)
+	pathBfs := graphs.BFS(&matrix, 0, 6)
+	pathDfs := graphs.DFS(&matrix, 0, 6)
 
-	if len(path) != len(expected) {
-		t.Log(path)
+	if len(pathBfs) != len(expected) {
 		t.Errorf("Graph matrix BFS failed, path is of incorrect length")
 	}
-	for idx := range path {
-		if path[idx] != expected[idx] {
+	for idx := range pathBfs {
+		if pathBfs[idx] != expected[idx] {
 			t.Errorf("Graph matrix BFS failed, path has incorrect steps")
 		}
 	}
 
+	if len(pathDfs) != len(expected) {
+		t.Errorf("Graph matrix DFS failed, path is of incorrect length")
+	}
+	for idx := range pathDfs {
+		if pathDfs[idx] != expected[idx] {
+			t.Errorf("Graph matrix DFS failed, path has incorrect steps")
+		}
+	}
+
 	matrix[5][6] = 0
-	path = graphs.Bfs(matrix, 0, 6)
-	if len(path) != 0 {
+	pathBfs = graphs.BFS(&matrix, 0, 6)
+	pathDfs = graphs.DFS(&matrix, 0, 6)
+	if len(pathBfs) != 0 {
 		t.Errorf("Graph matrix BFS failed, returned path when none exists")
+	}
+	if len(pathDfs) != 0 {
+		t.Errorf("Graph matrix DFS failed, returned path when none exists")
 	}
 }
