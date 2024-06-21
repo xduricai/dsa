ec517a70fb45b2421730cd1437c55202acf28ac0
xduricai <duricaigor@gmail.com>
2024-06-21 17:39:11 +0200

    Fixed bug + added an alternate solution to anagram groups

--- ts/arrays/anagram-groups.ts
+++ ts/arrays/anagram-groups.ts
@@ -1,11 +1,29 @@
-export function groupAnagrams(strs: string[]): string[][] {
+export function groupAnagrams(strs: string[]) {
+    const map = new Map<string, string[]>();
+    
+    for (let str of strs) {
+        const counts = Array(26).fill(0);
+        
+        for (let char of str.split("")) {
+            counts[char.charCodeAt(0) - 97]++;
+        }
+
+        const key = counts.toString();
+        const items = map.get(key) || [];
+        items.push(str);
+        map.set(key, items);
+    }
+    return [...map.values()];
+};
+
+export function groupAnagramsAlt(strs: string[]): string[][] {
     const map = new Map<string, string[]>();
     
     for (let str of strs) {
         const sorted = str.split("").sort().join("");
         const group = map.get(sorted);
 
-        if (!sorted) {
+        if (!group) {
             map.set(sorted, [str]);
             continue;
         }
