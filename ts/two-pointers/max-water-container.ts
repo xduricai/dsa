8ae37f055f99645e806ebba85e4162a924e37959
xduricai <duricaigor@gmail.com>
2024-06-25 18:22:39 +0200

    Max water container

---
+++ ts/two-pointers/max-water-container.ts
@@ -0,0 +1,26 @@
+export function maxArea(heights: number[]) {
+    let max = 0;
+    let low = 0;
+    let hi = heights.length - 1;
+
+    while (low < hi) {
+        if (heights[low] < heights[hi]) {
+            const area = heights[low] * (hi - low);
+            max = Math.max(area, max);
+            
+            const curr = heights[low];
+            while (heights[low] <= curr) {
+                low++;
+            }
+        } else {
+            const area = heights[hi] * (hi - low);
+            max = Math.max(area, max);
+            
+            const curr = heights[hi];
+            while (heights[hi] <= curr) {
+                hi--;
+            }
+        }
+    }
+return max;
+}
