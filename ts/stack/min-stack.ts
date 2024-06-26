1d4965171b8eb735f2e8822bf822d6c85b871b07
xduricai <duricaigor@gmail.com>
2024-06-26 17:32:09 +0200

    Added stack problems

---
+++ ts/stack/min-stack.ts
@@ -0,0 +1,25 @@
+class MinStack {
+    stack = [];
+    mins = [];
+
+    push(val: number): void {
+        this.stack.push(val);
+        
+        if (this.mins.length && this.mins[this.mins.length - 1] < val) 
+            this.mins.push(this.mins[this.mins.length - 1]);
+        else this.mins.push(val); 
+    }
+
+    pop(): void {
+        this.stack.pop();
+        this.mins.pop();
+    }
+
+    top(): number {
+        return this.stack[this.stack.length - 1];
+    }
+
+    getMin(): number {
+        return this.mins[this.stack.length - 1];
+    }
+}
---
+++ ts/stack/valid-parentheses.ts
@@ -0,0 +1,20 @@
+export function isValid(s: string) {
+    const stack = [];
+
+    for (let char of s.split("")) {
+        if (char === "(" || char === "{" || char === "[") {
+            stack.unshift(char);
+            continue;
+        }
+        const top = stack.shift();
+        
+        if (
+            char === ")" && top === "(" || 
+            char === "}" && top === "{" || 
+            char === "]" && top === "["
+        ) continue;
+
+        return false;
+    }
+    return stack.length === 0;
+}
