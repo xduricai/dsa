8342b24886a138cbdccdda1a71e7e989da1c0150
xduricai <duricaigor@gmail.com>
2024-06-27 20:15:03 +0200

    Added RPN

---
+++ ts/stack/rpn.ts
@@ -0,0 +1,18 @@
+export function evalRPN(tokens: string[]) {
+    const nums = [];
+
+    for (let token of tokens) {
+        if (!/^[+-/*]$/.test(token)) {
+            nums.push(parseInt(token));
+            continue;
+        }
+        const num = nums.pop();
+
+        if (token === "+") nums[nums.length - 1] += num;
+        else if (token === "*") nums[nums.length - 1] *= num ;
+        else if (token === "-") nums[nums.length - 1] -= num;
+        else if (token === "/") nums[nums.length - 1] = Math.trunc(nums[nums.length - 1] / num);
+        else return undefined;
+    }
+    return nums.pop();
+}
