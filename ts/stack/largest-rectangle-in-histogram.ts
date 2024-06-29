542bb6088d60095b8967d70b7c4a1195533d44ef
xduricai <duricaigor@gmail.com>
2024-06-29 17:57:53 +0200

    Added histogram area problem

---
+++ ts/stack/largest-rectangle-in-histogram.ts
@@ -0,0 +1,21 @@
+export function largestRectangleArea(heights: number[]) {
+    if (!heights.length) return 0;
+    const stack = [{ start: 0, height: heights[0] }];
+    let max = heights[0];
+
+    for (let idx = 1; idx < heights.length; idx++) {
+        const current = { start: idx, height: heights[idx] };
+        
+        while (stack.length && stack[stack.length - 1].height > heights[idx]) {
+            const top = stack.pop();
+            current.start = top.start;
+            max = Math.max(max, (idx - top.start) * top.height);
+        }
+        stack.push(current);
+    }
+
+    for (let item of stack) {
+        max = Math.max(max, (heights.length - item.start) * item.height);
+    }
+    return max;
+}
