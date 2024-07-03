20607da16742ac7ab87955899cbb1ddf668f57f8
xduricai <duricaigor@gmail.com>
2024-07-03 19:10:53 +0200

    Binary search + sliding window problem

---
+++ ts/binary-search/time-based-key-vault-store.ts
@@ -0,0 +1,36 @@
+export type TimeMapEntry = { value: string, timestamp: number };
+
+export class TimeMap {
+    keyStore: Map<string, TimeMapEntry[]>;
+    
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    set(key: string, value: string, timestamp: number) {
+        const arr = this.keyStore.get(key);
+        
+        if (!arr) this.keyStore.set(key, [{ value, timestamp }]);
+        else arr.push({ value, timestamp });
+    }
+
+    get(key: string, timestamp: number) {
+        const arr = this.keyStore.get(key);
+        if (!arr) return "";
+
+        let low = 0;
+        let hi = arr.length - 1;
+        let ret = "";
+
+        while (low <= hi) {
+            const mid = Math.floor((low + hi) / 2);
+            if (arr[mid].timestamp === timestamp) return arr[mid].value;
+
+            if (arr[mid].timestamp < timestamp){
+                ret = arr[mid].value;
+                low = mid + 1;
+            } else hi = mid - 1;
+        }
+        return ret;
+    }
+}
---
+++ ts/sliding-window/best-time-to-buy-stock.ts
@@ -0,0 +1,13 @@
+export function maxProfit(prices: number[]) {
+    let max = 0;
+    let left = 0;
+    let right = 1;
+
+    while (right < prices.length) {
+        if (prices[left] < prices[right]) 
+            max = Math.max(max, prices[right] - prices[left]);
+        else left = right;
+        right++;
+    }
+    return max;
+};
