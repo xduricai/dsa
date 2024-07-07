bc7a1114d296531127ca07e4f1ae9a20cc7ddcc8
xduricai <duricaigor@gmail.com>
2024-07-07 15:10:50 +0200

    Sliding window maximum

--- ts/sliding-window/minimum-window-substring.ts
+++ ts/sliding-window/minimum-window-substring.ts
@@ -1,17 +1,14 @@
-export function minWindow(s: string, t: string) {
-    if (t.length === 0 || t.length > s.length) return "";
-
+export function minWindow(s: string, t: string): string {
     const counts = new Map();
-    const window = new Map(); 
+    const window = new Map();
 
-    for (let char of t) {
+    for(let char of t) {
         counts.set(char, (counts.get(char) || 0) + 1);
     }
     const target = counts.size;
     let matches = 0;
-    let ret = "";
-    let retLen = Infinity;
     let left = 0;
+    let ret = "";
 
     for (let right = 0; right < s.length; right++) {
         window.set(s[right], (window.get(s[right]) || 0) + 1);
@@ -20,18 +17,17 @@ export function minWindow(s: string, t: string) {
             matches++;
         }
 
-        while (matches === target) {
-            if (right - left + 1 < retLen) {
+        while (matches === target) {     
+            if (right - left + 1 < ret.length || ret.length === 0) {
                 ret = s.slice(left, right + 1);
-                retLen = right - left + 1;
             }
-            window.set(s[left], window.get(s[left]) - 1);
 
-            if (counts.has(s[left]) && window.get(s[left]) < counts.get(s[left])) {
+            if(counts.has(s[left]) && counts.get(s[left]) === window.get(s[left])) {
                 matches--;
             }
+            window.set(s[left], window.get(s[left]) - 1);
             left++;
         }
     }
     return ret;
-}
+};
---
+++ ts/sliding-window/sliding-window-maximum.ts
@@ -0,0 +1,24 @@
+export function maxSlidingWindow(nums: number[], k: number) {
+    const output = [];
+    const queue = [];
+    
+    let left = 0;
+    let right = 0;
+
+    while (right < nums.length) {
+        while (queue.length > 0 && nums[queue[queue.length - 1]] < nums[right]) {
+            queue.pop();
+        }
+        queue.push(right);
+
+        if (left > queue[0]) {
+            queue.shift();
+        }
+        if (right + 1 >= k) {
+            output.push(nums[queue[0]]);
+            left++;
+        }
+        right++;
+    }
+    return output;
+}
