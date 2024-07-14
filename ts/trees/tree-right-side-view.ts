0841bccfd075594b073c137e356c02fcb71b2a09
xduricai <duricaigor@gmail.com>
2024-07-14 10:35:35 +0200

    Binary tree right side view

---
+++ ts/trees/tree-right-side-view.ts
@@ -0,0 +1,22 @@
+import { TreeNode } from "./tree-node";
+
+export function rightSideView(root: TreeNode | null): number[] {
+    const ret = [];
+    const queue = [root];
+
+    while (queue.length) {
+        let count = queue.length;
+        let right = null;
+
+        while(count) {
+            count--;
+            const node = queue.shift();
+            if (!node) continue;
+
+            right = node;
+            queue.push(node.left, node.right);
+        }
+        if (right) ret.push(right.val);
+    }
+    return ret;
+}
