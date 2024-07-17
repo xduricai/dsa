cd628cff8bb1fefa68c2d3cd87290a4df714a1f2
xduricai <duricaigor@gmail.com>
2024-07-17 15:25:49 +0200

    Maximum path sum, minor tweaks

---
+++ ts/trees/maximum-path-sum.ts
@@ -0,0 +1,20 @@
+import { TreeNode } from "./tree-node";
+
+export function maxPathSum(root: TreeNode | null): number {
+    let max = -Infinity;
+
+    const walk = (node: TreeNode | null): number => {
+        if (!node) return 0;
+
+        const left = walk(node.left);
+        const right = walk(node.right);
+        let nodeMax = node.val;
+
+        if (left > 0) nodeMax += left;
+        if (right > 0) nodeMax += right;
+        max = Math.max(nodeMax, max);
+        return node.val + Math.max(left, right, 0);
+    }
+    walk(root);
+    return max;
+}
--- ts/trees/tree-node.ts
+++ ts/trees/tree-node.ts
@@ -10,3 +10,25 @@ export class TreeNode {
         this.right = (right===undefined ? null : right)
     }
 }
+
+// utility function for level order non-formatted print
+export function print(root: TreeNode | null) {
+    if (!root) return;
+    const queue = [root];
+    let out = "";
+
+    while (queue.length) {
+        let count = queue.length;
+
+        while (count) {
+            count--;
+            const node = queue.shift();
+            out = `${out} ${node?.val.toString() || "N"}`;
+            if (node) {
+                queue.push(node.left, node.right);
+            }
+        }
+        out = `${out}\n`;
+    }
+    console.log(out);
+}
