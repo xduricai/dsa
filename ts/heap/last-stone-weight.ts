1fdeff8cbb9fd3425d20476511fa511884c30ca1
xduricai <duricaigor@gmail.com>
2024-07-27 18:19:05 +0200

    Last stone weight

---
+++ ts/heap/last-stone-weight.ts
@@ -0,0 +1,17 @@
+import { Heap } from "./heap";
+
+export function lastStoneWeight(stones: number[]) {
+    const heap = new Heap();
+    
+    for (let stone of stones) {
+        heap.add(-stone);
+    }
+
+    while (heap.length > 1) {
+        const a = heap.delete();
+        const b = heap.delete();
+        const res = Math.abs(a - b);
+        if (res) heap.add(-res);
+    }
+    return -heap.peek() || 0; 
+}
