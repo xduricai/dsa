585e6fec6b96e81ecd872af771970cb80487a90d
xduricai <duricaigor@gmail.com>
2024-07-28 15:30:28 +0200

    Added QuickSelect solution

--- ts/heap/k-th-largest-element.ts
+++ ts/heap/k-th-largest-element.ts
@@ -1,6 +1,36 @@
 import { Heap } from "./heap";
 
-export function findKthLargest(nums: number[], k: number): number {
+function findKthLargest(nums: number[], k: number): number {
+    k = nums.length - k;
+
+    const quickSelect = (left: number, right: number) => {
+        const pivot = nums[right];
+        let ptr = left;
+
+        for (let idx = left; idx < right; idx++) {
+            if (nums[idx] > pivot) continue;
+
+            const temp = nums[ptr];
+            nums[ptr] = nums[idx];
+            nums[idx] = temp;
+            ptr++;
+        }
+        const temp = nums[ptr];
+        nums[ptr] = nums[right];
+        nums[right] = temp;
+        
+        if (ptr > k) {
+            return quickSelect(left, ptr - 1);
+        }
+        if (ptr < k) {
+            return quickSelect(ptr + 1, right);
+        }
+        return nums[ptr];
+    }
+    return quickSelect(0, nums.length - 1);
+}
+
+export function findKthLargestAlt(nums: number[], k: number): number {
     const heap = new Heap<number>();
 
     for (let num of nums) {
