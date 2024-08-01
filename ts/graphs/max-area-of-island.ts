2cfe75ea7e46f606a9a9846ad521c1eefd2ea5e1
xduricai <duricaigor@gmail.com>
2024-08-01 17:20:26 +0200

    Number of islands + max area of island

---
+++ ts/graphs/max-area-of-island.ts
@@ -0,0 +1,34 @@
+function maxAreaOfIsland(grid: number[][]): number {
+    const seen = new Set<string>();
+    let max = 0;
+
+    const inBounds = (row: number, col: number) => {
+        return row >= 0 && col >= 0 && row < grid.length && col < grid[0].length;
+    }
+
+    const dfs = (row: number, col: number) => {
+        if (!inBounds(row, col) || seen.has(`${row}-${col}`) || grid[row][col] === 0) {
+            return 0;
+        }
+        seen.add(`${row}-${col}`);
+        
+        return 1 
+            + dfs(row - 1, col)
+            + dfs(row, col + 1)
+            + dfs(row + 1, col)
+            + dfs(row, col - 1);
+    }
+
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[0].length; col++) {
+            if (seen.has(`${row}-${col}`)) continue;
+
+            if (grid[row][col] === 1) {
+                max = Math.max(max, dfs(row, col));
+            } else {
+                seen.add(`${row}-${col}`);
+            }
+        }
+    }
+    return max;
+};
---
+++ ts/graphs/number-of-islands.ts
@@ -0,0 +1,36 @@
+function numIslands(grid: string[][]): number {
+    const seen = Array(grid.length)
+        .fill(null)
+        .map(_ => Array(grid[0].length).fill(false));
+    let islands = 0;
+
+    const inBounds = (row: number, col: number) => {
+        return row >= 0 && col >= 0 && row < grid.length && col < grid[0].length;
+    }
+
+    const dfs = (row: number, col: number) => {
+        if (!inBounds(row, col) || seen[row][col] || grid[row][col] === "0") {
+            return;
+        }
+        seen[row][col] = true;
+
+        dfs(row - 1, col);
+        dfs(row, col + 1);
+        dfs(row + 1, col);
+        dfs(row, col - 1);
+    }
+
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[0].length; col++) {
+            if (seen[row][col]) continue;
+
+            if (grid[row][col] === "1") {
+                islands++;
+                dfs(row, col);
+            } else {
+                seen[row][col] = true;
+            }
+        }
+    }
+    return islands;
+};
