23cc1e84c7a4cb0c2b25ef520cbcb7d16752da3d
xduricai <duricaigor@gmail.com>
2024-08-24 16:01:17 +0200

    Reconstruct itinerary

---
+++ ts/graphs-advanced/reconstruct-itinerary.ts
@@ -0,0 +1,30 @@
+export function findItinerary(tickets: string[][]): string[] {
+    const adjList = new Map<string, string[]>();
+    const path = [];
+
+    for (const [source, destination] of tickets) {
+        const current = adjList.get(source);
+        if (current) {
+            current.push(destination);
+        } else {
+            adjList.set(source, [destination]);
+        }
+    }
+
+    for (const list of adjList.values()) {
+        list.sort();
+    }
+
+    const dfs = (source: string) => {
+        const neighbors = adjList.get(source) || [];
+
+        while (neighbors.length) {
+            const neighbor = neighbors.shift();
+            dfs(neighbor);
+        }
+        path.push(source);
+    };
+
+    dfs("JFK");
+    return path.reverse();
+}
