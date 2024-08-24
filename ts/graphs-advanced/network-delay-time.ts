b7088dccd5d20cd52f6a0b6f6748dc895e7f8959
xduricai <duricaigor@gmail.com>
2024-08-24 20:07:56 +0200

    Network delay time

---
+++ ts/graphs-advanced/network-delay-time.ts
@@ -0,0 +1,39 @@
+import { Heap } from "../heap/heap";
+
+export function networkDelayTime(
+    times: number[][],
+    n: number,
+    k: number
+): number {
+    const visited = new Set<number>();
+    const adjList = new Map<number, [number, number][]>();
+    const heap = new Heap<[number, number]>();
+    let total = 0;
+
+    for (const [source, destination, cost] of times) {
+        const neighbors = adjList.get(source);
+        if (neighbors) {
+            neighbors.push([cost, destination]);
+        } else {
+            adjList.set(source, [[cost, destination]]);
+        }
+    }
+    heap.add([0, k]);
+
+    while (heap.length) {
+        const [cost, node] = heap.delete();
+        if (visited.has(node)) {
+            continue;
+        }
+        visited.add(node);
+        total = Math.max(total, cost);
+
+        for (const [neighborCost, neighbor] of adjList.get(node) || []) {
+            if (visited.has(neighbor)) {
+                continue;
+            }
+            heap.add([cost + neighborCost, neighbor]);
+        }
+    }
+    return visited.size === n ? total : -1;
+}
