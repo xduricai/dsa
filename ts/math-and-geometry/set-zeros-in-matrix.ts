06714588905e4ee1a76761aadeaf4e740ca15558
xduricai <duricaigor@gmail.com>
2024-08-25 17:39:39 +0200

    Set zeros in matrix

---
+++ ts/math-and-geometry/set-zeros-in-matrix.ts
@@ -0,0 +1,38 @@
+export function setZeroes(matrix: number[][]): void {
+    const ROWS = matrix.length;
+    const COLS = matrix[0].length;
+    let firstRow = false;
+
+    for (let row = 0; row < ROWS; row++) {
+        for (let col = 0; col < COLS; col++) {
+            if (matrix[row][col] === 0) {
+                matrix[0][col] = 0;
+
+                if (row > 0) {
+                    matrix[row][0] = 0;
+                } else {
+                    firstRow = true;
+                }
+            }
+        }
+    }
+
+    for (let row = 1; row < ROWS; row++) {
+        for (let col = 1; col < COLS; col++) {
+            if (matrix[0][col] === 0 || matrix[row][0] === 0) {
+                matrix[row][col] = 0;
+            }
+        }
+    }
+
+    if (matrix[0][0] === 0) {
+        for (let row = 0; row < ROWS; row++) {
+            matrix[row][0] = 0;
+        }
+    }
+    if (firstRow) {
+        for (let col = 0; col < COLS; col++) {
+            matrix[0][col] = 0;
+        }
+    }
+}
