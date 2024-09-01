c04bfeace7babcc82634237ad6af40ba0fdd4fc7
xduricai <duricaigor@gmail.com>
2024-09-01 15:07:47 +0200

    Insertion sort

---
+++ ts/sorting/insertion-sort.ts
@@ -0,0 +1,22 @@
+type Pair = {
+    key: any;
+    number: any;
+};
+
+export function insertionSort(pairs: Pair[]) {
+    const states: Pair[][] = [];
+
+    for (let iter = 0; iter < pairs.length; iter++) {
+        for (let idx = iter; idx > 0; idx--) {
+            if (pairs[idx].key >= pairs[idx - 1].key) {
+                break;
+            }
+
+            const temp = pairs[idx];
+            pairs[idx] = pairs[idx - 1];
+            pairs[idx - 1] = temp;
+        }
+        states.push([...pairs]);
+    }
+    return states;
+}
