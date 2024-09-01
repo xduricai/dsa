6d9f6e8c34049639595863891c26982b0c8bc6e4
xduricai <duricaigor@gmail.com>
2024-09-01 16:27:26 +0200

    Quick sort

---
+++ ts/sorting/quick-sort.ts
@@ -0,0 +1,30 @@
+import { Pair } from "./pair";
+
+export function quickSort(pairs: Pair[]) {
+    this.qs(pairs, 0, pairs.length - 1);
+    return pairs;
+}
+
+function qs(pairs: Pair[], left: number, right: number) {
+    if (left >= right) {
+        return;
+    }
+    const pivot = pairs[right];
+    let pIdx = left;
+
+    for (let idx = left; idx < right; idx++) {
+        if (pairs[idx].key >= pivot.key) {
+            continue;
+        }
+        const temp = pairs[pIdx];
+        pairs[pIdx] = pairs[idx];
+        pairs[idx] = temp;
+
+        pIdx++;
+    }
+    pairs[right] = pairs[pIdx];
+    pairs[pIdx] = pivot;
+
+    this.qs(pairs, left, pIdx - 1);
+    this.qs(pairs, pIdx + 1, right);
+}
