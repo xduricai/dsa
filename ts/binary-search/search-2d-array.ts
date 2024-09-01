79ec2d91370b5cdedcedc8dcc90bc23c2bb07f37
xduricai <duricaigor@gmail.com>
2024-09-01 18:43:26 +0200

    Search 2D array alternate solution

--- ts/binary-search/search-2d-array.ts
+++ ts/binary-search/search-2d-array.ts
@@ -30,3 +30,32 @@ export function searchMatrix(matrix: number[][], target: number) {
     }
     return false;
 }
+
+// if every row is of the same length
+function searchMatrixAlt(matrix: number[][], target: number) {
+    const ROWS = matrix.length;
+    const COLS = matrix[0].length;
+
+    const getPos = (mid: number) => {
+        const row = Math.floor(mid / COLS);
+        const col = mid % COLS;
+        return [row, col];
+    };
+
+    let low = 0;
+    let hi = ROWS * COLS - 1;
+
+    while (low <= hi) {
+        const mid = Math.floor((hi + low) / 2);
+        const [row, col] = getPos(mid);
+
+        if (matrix[row][col] > target) {
+            hi = mid - 1;
+        } else if (matrix[row][col] < target) {
+            low = mid + 1;
+        } else {
+            return true;
+        }
+    }
+    return false;
+}
