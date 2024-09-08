2e09596611063d744337ccd1f691bf6f1c300ad1
xduricai <duricaigor@gmail.com>
2024-09-08 15:39:30 +0200

    Matrix DFS

---
+++ ts/graphs/matrix-dfs.ts
@@ -0,0 +1,32 @@
+export function countPaths(grid: number[][]) {
+    const ROWS = grid.length;
+    const COLS = grid[0].length;
+    const seen = new Set();
+
+    const inBounds = (row: number, col: number) => {
+        return row >= 0 && col >= 0 && row < ROWS && col < COLS;
+    };
+
+    const dfs = (row: number, col: number) => {
+        const key = `${row}-${col}`;
+
+        if (!inBounds(row, col) || grid[row][col] === 1 || seen.has(key)) {
+            return 0;
+        }
+        if (row === ROWS - 1 && col === COLS - 1) {
+            return 1;
+        }
+
+        seen.add(key);
+        let count = 0;
+
+        count += dfs(row - 1, col);
+        count += dfs(row, col + 1);
+        count += dfs(row + 1, col);
+        count += dfs(row, col - 1);
+
+        seen.delete(key);
+        return count;
+    };
+    return dfs(0, 0);
+}
