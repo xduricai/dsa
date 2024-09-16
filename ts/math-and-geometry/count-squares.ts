a9d75b21f1b14fe4e6ecdf9c1cc870dda92328b3
xduricai <duricaigor@gmail.com>
2024-09-16 17:46:41 +0200

    Detect squares alt version

--- ts/math-and-geometry/count-squares.ts
+++ ts/math-and-geometry/count-squares.ts
@@ -24,3 +24,50 @@ export class DetectSquares {
         return res;
     }
 }
+
+// les space efficient solution using diagonals
+export class DetectSquaresAlt {
+    counts = new Map<string, number>();
+    posDiagonals = new Map<number, number[][]>();
+    negDiagonals = new Map<number, number[][]>();
+
+    add(point: number[]): void {
+        const [px, py] = point;
+        const key = `${px}-${py}`;
+        this.counts.set(key, (this.counts.get(key) || 0) + 1);
+
+        const posDiagonal = this.posDiagonals.get(px + py);
+        if (posDiagonal) {
+            posDiagonal.push(point);
+        } else {
+            this.posDiagonals.set(px + py, [point]);
+        }
+
+        const negDiagonal = this.negDiagonals.get(px - py);
+        if (negDiagonal) {
+            negDiagonal.push(point);
+        } else {
+            this.negDiagonals.set(px - py, [point]);
+        }
+    }
+
+    count(point: number[]): number {
+        const [px, py] = point;
+        const pos = this.posDiagonals.get(px + py) || [];
+        const neg = this.negDiagonals.get(px - py) || [];
+        const points = pos.concat(neg);
+
+        let count = 0;
+
+        for (const [x, y] of points) {
+            if (x === px) {
+                continue;
+            }
+
+            const countA = this.counts.get(`${px}-${y}`) || 0;
+            const countB = this.counts.get(`${x}-${py}`) || 0;
+            count += countA * countB;
+        }
+        return count;
+    }
+}
