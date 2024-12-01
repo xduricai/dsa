f5658e6da61e51260b7badfadfac9628ba4b2a0c
xduricai <duricaigor@gmail.com>
2024-12-01 17:35:25 +0100

    Added more readable implementation of coin change I

--- ts/1D-DP/coin-change.ts
+++ ts/1D-DP/coin-change.ts
@@ -2,6 +2,22 @@ export function coinChange(coins: number[], amount: number): number {
     const dp = Array(amount + 1).fill(Infinity);
     dp[0] = 0;
 
+    for (const coin of coins) {
+        for (let target = coin; target <= amount; target++) {
+            dp[target] = Math.min(dp[target], dp[target - coin] + 1);
+        }
+    }
+
+    if (dp[amount] === Infinity) {
+        return -1;
+    }
+    return dp[amount];
+}
+
+export function coinChangeAlt(coins: number[], amount: number): number {
+    const dp = Array(amount + 1).fill(Infinity);
+    dp[0] = 0;
+
     for (let target = 1; target <= amount; target++) {
         for (let coin of coins) {
             if (target - coin >= 0) {
