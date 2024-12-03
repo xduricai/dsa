00ee28e90b7cb2d763a2a08af4f51cc2331678e8
xduricai <duricaigor@gmail.com>
2024-12-03 17:31:37 +0100

    Alternate solution to distinct subsequences

--- ts/2D-DP/distinct-subsequences.ts
+++ ts/2D-DP/distinct-subsequences.ts
@@ -1,4 +1,25 @@
+// more compact solution
 export function numDistinct(s: string, t: string): number {
+    let dp = Array(t.length + 1).fill(0);
+    dp[0] = 1;
+
+    for (let row = 1; row <= s.length; row++) {
+        const current = Array(t.length + 1).fill(0);
+        current[0] = 1;
+
+        for (let col = 1; col <= row; col++) {
+            current[col] = dp[col];
+
+            if (s[row - 1] === t[col - 1]) {
+                current[col] += dp[col - 1];
+            }
+        }
+        dp = current;
+    }
+    return dp[t.length];
+}
+
+export function numDistinctAlt(s: string, t: string): number {
     const dp = Array(s.length + 1)
         .fill(null)
         .map((_) => Array(t.length + 1).fill(0));
