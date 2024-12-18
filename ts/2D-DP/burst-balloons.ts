6e05a9e418408386cc3777d001b67c3cd728abb5
xduricai <duricaigor@gmail.com>
2024-12-18 17:10:11 +0100

    Burst balloons optimal solution

--- ts/2D-DP/burst-balloons.ts
+++ ts/2D-DP/burst-balloons.ts
@@ -1,6 +1,36 @@
 export function maxCoins(nums: number[]): number {
     nums.unshift(1);
     nums.push(1);
+    const N = nums.length;
+    const dp: number[][] = Array.from({ length: N }, () =>
+        new Array(N).fill(0)
+    );
+
+    // length of the window must be at least 2 and less than N
+    for (let length = 2; length < N; length++) {
+        // check all L/R pairs for current window size
+        for (let left = 0; left < N - length; left++) {
+            const right = left + length;
+
+            for (let idx = left + 1; idx < right; idx++) {
+                // calculate coin gain for the current window
+                const coins =
+                    dp[left][idx] +
+                    dp[idx][right] +
+                    nums[left] * nums[idx] * nums[right];
+
+                // update current cell if needed
+                dp[left][right] = Math.max(dp[left][right], coins);
+            }
+        }
+    }
+
+    return dp[0][N - 1];
+}
+
+export function maxCoinsAlt(nums: number[]): number {
+    nums.unshift(1);
+    nums.push(1);
 
     const dp = Array(nums.length)
         .fill(null)
@@ -26,7 +56,7 @@ export function maxCoins(nums: number[]): number {
     return dfs(1, nums.length - 2);
 }
 
-export function maxCoinsAlt(nums: number[]): number {
+export function maxCoinsDfs(nums: number[]): number {
     const dp = new Map();
     nums.unshift(1);
     nums.push(1);
