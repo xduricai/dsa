f55c999349485769cc76420ee231096ba3443e3d
xduricai <duricaigor@gmail.com>
2024-12-29 10:47:00 +0100

    Number of ways to form target string from dictionary

---
+++ ts/1D-DP/number-of-ways-to-form-string-from-dictionary.ts
@@ -0,0 +1,27 @@
+function numWays(words: string[], target: string): number {
+    const PRIME = 10 ** 9 + 7;
+    const A = "a".charCodeAt(0);
+
+    const dp = Array(target.length + 1).fill(0);
+    dp[0] = 1;
+
+    // iterate over character slots in the dictionary
+    for (let idx = 0; idx < words[0].length; idx++) {
+        const counter = Array(26).fill(0);
+
+        // count the character occurrences in a given slot
+        for (const word of words) {
+            const charIdx = word[idx].charCodeAt(0) - A;
+            counter[charIdx]++;
+        }
+
+        // compute results for every position in target
+        for (let targetIdx = target.length; targetIdx > 0; targetIdx--) {
+            const charIdx = target[targetIdx - 1].charCodeAt(0) - A;
+            dp[targetIdx] =
+                (dp[targetIdx] + dp[targetIdx - 1] * counter[charIdx]) % PRIME;
+        }
+    }
+
+    return dp.at(-1) % PRIME;
+}
