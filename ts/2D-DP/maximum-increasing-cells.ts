240966df2b7f6869b08d7d7f09c1c831c15da3de
xduricai <duricaigor@gmail.com>
2024-12-29 17:57:49 +0100

    Added DFS solution to strictly increasing cells in matrix

--- ts/2D-DP/maximum-increasing-cells.ts
+++ ts/2D-DP/maximum-increasing-cells.ts
@@ -44,3 +44,57 @@ export function maxIncreasingCells(mat: number[][]): number {
 
     return res;
 }
+
+// suboptimal solution, TLE
+export function maxIncreasingCellsDfs(mat: number[][]): number {
+    const ROWS = mat.length;
+    const COLS = mat[0].length;
+
+    const valueMap = new Map<number, [number, number][]>();
+    const DP = Array.from({ length: ROWS }, () => Array(COLS).fill(0));
+    let max = 0;
+
+    // map values onto row/col combinations where they occur
+    for (let row = 0; row < ROWS; row++) {
+        for (let col = 0; col < COLS; col++) {
+            const list = valueMap.get(mat[row][col]);
+
+            if (list) {
+                list.push([row, col]);
+            } else {
+                valueMap.set(mat[row][col], [[row, col]]);
+            }
+        }
+    }
+
+    const dfs = (row: number, col: number) => {
+        if (DP[row][col]) {
+            return DP[row][col];
+        }
+
+        let res = 0;
+
+        for (let r = 0; r < ROWS; r++) {
+            if (mat[r][col] > mat[row][col]) {
+                res = Math.max(res, dfs(r, col));
+            }
+        }
+
+        for (let c = 0; c < COLS; c++) {
+            if (mat[row][c] > mat[row][col]) {
+                res = Math.max(res, dfs(row, c));
+            }
+        }
+
+        DP[row][col] = res + 1;
+        return DP[row][col];
+    };
+
+    for (const key of Array.from(valueMap.keys()).sort((a, b) => a - b)) {
+        for (const [row, col] of valueMap.get(key)) {
+            max = Math.max(max, dfs(row, col));
+        }
+    }
+
+    return max;
+}
