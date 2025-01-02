b131695a364382e3b30e38ad6c56f0855ee428f1
xduricai <duricaigor@gmail.com>
2025-01-02 11:36:46 +0100

    Perfect squares

---
+++ ts/1D-DP/perfect-squares.ts
@@ -0,0 +1,17 @@
+export function numSquares(n: number): number {
+    // in the worst case, every n can be written as a sum of n 1's
+    const DP = Array.from({ length: n + 1 }, (_, idx) => idx);
+    const squares = [1];
+
+    for (let num = 2; num <= Math.floor(Math.sqrt(n)); num++) {
+        squares.push(num * num);
+    }
+
+    for (const num of squares) {
+        for (let idx = num; idx <= n; idx++) {
+            DP[idx] = Math.min(DP[idx], DP[idx - num] + 1);
+        }
+    }
+
+    return DP[n];
+}
