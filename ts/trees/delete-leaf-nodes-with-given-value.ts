dcf82c99b8f51262db8974c4af53001fd2ca62f2
xduricai <duricaigor@gmail.com>
2025-01-13 18:29:12 +0100

    Delete leaf nodes with a given value

---
+++ ts/trees/delete-leaf-nodes-with-given-value.ts
@@ -0,0 +1,19 @@
+import { TreeNode } from "./tree-node";
+
+export function removeLeafNodes(
+    root: TreeNode | null,
+    target: number
+): TreeNode | null {
+    if (!root) {
+        return null;
+    }
+
+    root.left = removeLeafNodes(root.left, target);
+    root.right = removeLeafNodes(root.right, target);
+
+    if (!root.left && !root.right && root.val === target) {
+        return null;
+    }
+
+    return root;
+}
