677ed4d2a7be5aed308c6047c4c808510844ba15
xduricai <duricaigor@gmail.com>
2025-03-05 16:30:17 +0100

    Count the number of colored cells

---
+++ ts/math-and-geometry/number-of-colored-cells.ts
@@ -0,0 +1,17 @@
+// O(1) solution
+export function coloredCells(n: number): number {
+    return 1 + 2 * n * n - 2 * n;
+}
+
+// suboptimal O(n) solution
+export function coloredCellsAlt(n: number): number {
+    let res = 1;
+    let add = 4;
+
+    for (let iter = 1; iter < n; iter++) {
+        res += add;
+        add += 4;
+    }
+
+    return res;
+}
