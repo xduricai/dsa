b96aa9375f391b319b197e22350f7b5600fc1ce2
xduricai <duricaigor@gmail.com>
2025-03-24 16:39:53 +0100

    Count days without meetings

---
+++ ts/intervals/days-without-meetings.ts
@@ -0,0 +1,22 @@
+export function countDays(days: number, meetings: number[][]): number {
+    meetings.sort((a, b) => a[0] - b[0]);
+    const merged = [meetings[0]];
+    let res = days;
+
+    for (let idx = 1; idx < meetings.length; idx++) {
+        if (meetings[idx][0] <= merged[merged.length - 1][1]) {
+            merged[merged.length - 1][1] = Math.max(
+                merged[merged.length - 1][1],
+                meetings[idx][1]
+            );
+        } else {
+            merged.push(meetings[idx]);
+        }
+    }
+
+    for (const [start, end] of merged) {
+        res -= end - start + 1;
+    }
+
+    return res;
+}
