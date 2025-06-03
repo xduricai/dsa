4ae485b53c23d1a24ba9369e8483f2a2643872b8
xduricai <duricaigor@gmail.com>
2025-06-03 16:38:20 +0200

    Added bfs solution to 1298

--- ts/graphs/maximum-candies-from-boxes.ts
+++ ts/graphs/maximum-candies-from-boxes.ts
@@ -7,6 +7,42 @@ export function maxCandies(
     containedBoxes: number[][],
     initialBoxes: number[]
 ): number {
+    let boxes = [...initialBoxes];
+    let done = false;
+    let res = 0;
+
+    while (!done) {
+        done = true;
+        const nextBoxes = [];
+
+        for (const box of boxes) {
+            if (!status[box]) {
+                nextBoxes.push(box);
+                continue;
+            }
+            done = false;
+
+            for (const contained of containedBoxes[box]) {
+                nextBoxes.push(contained);
+            }
+            for (const key of keys[box]) {
+                status[key] = 1;
+            }
+            res += candies[box];
+        }
+        boxes = nextBoxes;
+    }
+
+    return res;
+}
+
+export function maxCandiesAlt(
+    status: number[],
+    candies: number[],
+    keys: number[][],
+    containedBoxes: number[][],
+    initialBoxes: number[]
+): number {
     const closed = new Set<number>();
     const keySet = new Set<number>();
     let res = 0;
