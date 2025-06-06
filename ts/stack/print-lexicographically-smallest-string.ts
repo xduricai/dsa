603b44c6f3c27245e00915b3215772d505b150a4
xduricai <duricaigor@gmail.com>
2025-06-06 15:53:56 +0200

    Print lexicographically smallest string

---
+++ ts/stack/print-lexicographically-smallest-string.ts
@@ -0,0 +1,31 @@
+// LC 2434 (https://leetcode.com/problems/using-a-robot-to-print-the-lexicographically-smallest-string)
+
+export function robotWithString(s: string): string {
+    const a = "a".charCodeAt(0);
+    const counter = new Array(26).fill(0);
+    const stack: string[] = [];
+    const res: string[] = [];
+
+    for (const char of s) {
+        counter[char.charCodeAt(0) - a]++;
+    }
+
+    for (const char of s) {
+        stack.push(char);
+        counter[char.charCodeAt(0) - a]--;
+        let min = String.fromCharCode(a + 26);
+
+        for (let idx = 0; idx < 26; idx++) {
+            if (counter[idx]) {
+                min = String.fromCharCode(idx + a);
+                break;
+            }
+        }
+
+        while (stack.length && stack.at(-1) <= min) {
+            res.push(stack.pop());
+        }
+    }
+
+    return res.join("");
+}
